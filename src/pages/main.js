43059d984f77977586e53be933f8a1c1f8cbe8ab
XandyGomes <x3.computadores@hotmail.com>
2024-10-23 10:12:22 -0300

    Aula.23.10.2024

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,12 +1,30 @@
-import React, {Component} from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
-export default class Main extends Component {
-  render() {
-    return (
-      <Container>
-        <Text>Teste</Text>
-      </Container>
-    );
-  }
+import React, {useState} from 'react';
+import MapView from 'react-native-maps';
+import useLocation from '../hooks/useLocation';
+
+export default function Main({navigation}) {
+  const [latitude, setLatitude] = useState(-20.398259);
+  const [longitude, setLongitude] = useState(-43.507726);
+
+  const {coords, errorMsg} = useLocation();
+
+  return (
+    <MapView
+      showsUserLocation={true}
+      showsMyLocationButton={false}
+      toolbarEnabled={false}
+      style={{
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+      }}
+      initialRegion={{
+        latitude,
+        longitude,
+        latitudeDelta: 0.010,
+        longitudeDelta: 0.010,
+        ...coords,
+      }}
+    />
+  );
 }
