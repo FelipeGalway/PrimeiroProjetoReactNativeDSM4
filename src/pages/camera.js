9ca0cc5db687eeaa2a11ae80fddb526c538b9575
XandyGomes <x3.computadores@hotmail.com>
2024-11-14 17:41:35 -0300

    Aula.14.11.2024

--- src/pages/camera.js
+++ src/pages/camera.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -10,14 +11,21 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {
+  launchCamera, // Função para abrir a câmera
+  launchImageLibrary, // Função para abrir a biblioteca de imagens
+} from 'react-native-image-picker';
 
+// Declaração do componente principal
 const Camera = () => {
+  // Define um estado para armazenar informações do arquivo selecionado
   const [filePath, setFilePath] = useState({});
 
+  // Função para solicitar permissão de acesso à câmera (apenas para Android)
   const requestCameraPermission = async () => {
     if (Platform.OS === 'android') {
       try {
+        // Tenta solicitar permissão para usar a câmera
         const granted = await PermissionsAndroid.request(
           PermissionsAndroid.PERMISSIONS.CAMERA,
           {
@@ -25,20 +33,21 @@ const Camera = () => {
             message: 'O aplicativo precisa de permissão para acessar a câmera',
           },
         );
+        // Se a permissão da câmera for concedida, retorna verdadeiro
         return granted === PermissionsAndroid.RESULTS.GRANTED;
       } catch (err) {
         console.warn(err);
         alert('Erro de permissão de câmera', err);
         return false;
       }
-    } else {
-      return true;
-    }
+    } else return true; // Se a plataforma não for Android, retorna verdadeiro
   };
 
+  // Função para solicitar permissão de escrita externa (apenas para Android)
   const requestExternalWritePermission = async () => {
     if (Platform.OS === 'android') {
       try {
+        // Tenta solicitar permissão para escrever em armazenamento externo
         const granted = await PermissionsAndroid.request(
           PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
           {
@@ -47,37 +56,40 @@ const Camera = () => {
               'O aplicativo precisa de permissão para escrever em armazenamento externo',
           },
         );
+        // Se a permissão para escrita externa for concedida, retorna verdadeiro
         return granted === PermissionsAndroid.RESULTS.GRANTED;
       } catch (err) {
         console.warn(err);
         alert('Erro de permissão de escrita', err);
-        return false;
       }
-    } else {
-      return true;
-    }
+      return false;
+    } else return true;
   };
 
+  // Função para capturar uma imagem da câmera
   const captureImage = async type => {
     let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeigth: 550,
-      quality: 1,
-      videoQuality: 'low',
-      durationLimit: 30,
-      saveToPhotos: true,
+      mediaType: type, // Tipo de mídia a ser capturada (foto ou vídeo)
+      maxWidth: 300, // Largura máxima da imagem
+      maxHeight: 550, // Altura máxima da imagem
+      quality: 1, // Qualidade da imagem (0 a 1)
+      videoQuality: 'low', // Qualidade do vídeo (baixa)
+      durationLimit: 30, // Duração máxima do vídeo em segundos
+      saveToPhotos: true, // Salvar na galeria de fotos
     };
-    let isCameraPermitted = await requestCameraPermission();
-    let isStoragePermitted = await requestExternalWritePermission();
-    if (isCameraPermitted && isStoragePermitted) {
+
+    let isCameraPermitted = await requestCameraPermission(); // Verifica permissão da câmera
+    // let isStoragePermitted = await requestExternalWritePermission(); // Verifica permissão de escrita externa
+    if (isCameraPermitted) {
+      // Se ambas as permissões forem concedidas, abre a câmera
       launchCamera(options, response => {
         console.log('Resposta = ', response);
+
         if (response.didCancel) {
           alert('O usuário cancelou a seleção da câmera');
           return;
         } else if (response.errorCode == 'camera_unavailable') {
-          alert('Câmera não disponivel no dispositivo');
+          alert('Câmera não disponível no dispositivo');
           return;
         } else if (response.errorCode == 'permission') {
           alert('Permissão não concedida');
@@ -91,21 +103,22 @@ const Camera = () => {
     }
   };
 
+  // Função para escolher um arquivo da biblioteca de imagens
   const chooseFile = type => {
     let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeigth: 550,
-      quality: 1,
+      mediaType: type, // Tipo de mídia a ser selecionada (foto ou vídeo)
+      maxWidth: 300, // Largura máxima da imagem
+      maxHeight: 550, // Altura máxima da imagem
+      quality: 1, // Qualidade da imagem (0 a 1)
     };
     launchImageLibrary(options, response => {
-      console.log('Resposta =', response);
+      console.log('Resposta = ', response);
 
       if (response.didCancel) {
         alert('O usuário cancelou a seleção da câmera');
         return;
       } else if (response.errorCode == 'camera_unavailable') {
-        alert('Câmera não disponivel no dispositivo');
+        alert('Câmera não disponível no dispositivo');
         return;
       } else if (response.errorCode == 'permission') {
         alert('Permissão não concedida');
@@ -118,6 +131,7 @@ const Camera = () => {
     });
   };
 
+  // Componente de interface do usuário
   return (
     <SafeAreaView style={{flex: 1}}>
       <Text style={styles.titleText}>
@@ -128,56 +142,57 @@ const Camera = () => {
           activeOpacity={0.5}
           style={styles.buttonStyle}
           onPress={() => captureImage('photo')}>
-          <Text style={styles.textStyle}>CÂMERA FOTO</Text>
+          <Text style={styles.textStyle}>Abrir Câmera para Capturar Foto</Text>
         </TouchableOpacity>
         <TouchableOpacity
           activeOpacity={0.5}
           style={styles.buttonStyle}
           onPress={() => captureImage('video')}>
-          <Text style={styles.textStyle}>CÂMERA VÍDEO</Text>
-        </TouchableOpacity>        
+          <Text style={styles.textStyle}>Abrir Câmera para Capturar Vídeo</Text>
+        </TouchableOpacity>
         <TouchableOpacity
           activeOpacity={0.5}
           style={styles.buttonStyle}
           onPress={() => chooseFile('photo')}>
-          <Text style={styles.textStyle}>GALERIA FOTO</Text>
-        </TouchableOpacity>        
+          <Text style={styles.textStyle}>Galeria de Imagens</Text>
+        </TouchableOpacity>
         <TouchableOpacity
           activeOpacity={0.5}
           style={styles.buttonStyle}
           onPress={() => chooseFile('video')}>
-          <Text style={styles.textStyle}>GALERIA VÍDEO</Text>
-        </TouchableOpacity>        
+          <Text style={styles.textStyle}>Galeria de Vídeos</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
 };
 
+// Estilos para os componentes
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        padding: 10,
-        backgroundColor: '#fff',
-        alignItems: 'center'
-    },
-    titleText: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        paddingVertical: 20,
-    },
-    textStyle: {
-        padding: 10,
-        color: 'black',
-        textAlign: 'center'
-    },
-    buttonStyle: {
-        alignItems: 'center',
-        backgroundColor: '#ADD8E6',
-        padding: 5,
-        marginVertical: 10,
-        width: 250,
-    }
-})
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    padding: 10,
+    color: 'black',
+    textAlign: 'center',
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 5,
+    marginVertical: 10,
+    width: 250,
+  },
+});
 
 export default Camera;
